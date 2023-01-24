27a575267aed6c6ab8e1f4b814bd25f70b5dc484
Nickolans Griffith <nickolansgriffith@gmail.com>
2023-01-24 01:56:34 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EasyBLE
+
+A description of this package.
---
+++ Tests/EasyBLETests/EasyBLETests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import EasyBLE
+
+final class EasyBLETests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(EasyBLE().text, "Hello, World!")
+    }
+}
